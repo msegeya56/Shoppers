9b68b0045fdce01ab43a9eedb462ed57fa89ba42
Paul Msegeya <92868240+pmtabe1@users.noreply.github.com>
2023-04-05 09:20:38 -0400

    ppackage main
    import()
    
    //we registry service by register a customer
    type Customer struct{}
    
    type Product struct{}
    
    type ProductCategory struct{}
    
    type Order struct {}
    
    type OrderProduct struct{}
    
    type Payment struct{}
    
    func main(){
    	//Initializing the database
    
    	//create a router
    
    	// r:=gin.Deafault
    
    	//initialize the endpoints
    	//r.GET("/customers",registryService.GetCustomers)
    	//r.POST("/products",productService.CreateProduct)
    	//r.PUT("/customers",customerService.UpdateCustomers)
    	//r.Get("/orders",orderService.GetOrder)
    	//r.Post("/orders",orderService.UpddateOrder)
    	//r.GET("/paymeent",paymentSErvice.GetPayment)
    //r.POST("/payement",paymentService.CreatePayment)
    	//
    	//
    	//Start the server aand connect the service
    
    }

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import()
+
+//we registry service by register a customer
+type Customer struct{}
+
+
+type Product struct{}
+
+type ProductCategory struct{}
+
+type Order struct {}
+
+type OrderProduct struct{}
+
+type Payment struct{}
+
+
+
+
+
+
+
+
+
+func main(){
+	//Initializing the database
+
+
+	//create a router
+
+	// r:=gin.Deafault
+
+
+	//initialize the endpoints
+	//r.GET("/customers",registryService.GetCustomers)
+	//r.POST("/products",productService.CreateProduct)
+	//r.PUT("/customers",customerService.UpdateCustomers)
+	//r.Get("/orders",orderService.GetOrder)
+	//r.Post("/orders",orderService.UpddateOrder)
+	//r.GET("/paymeent",paymentSErvice.GetPayment)
+//r.POST("/payement",paymentService.CreatePayment)
+	//
+	//
+	//Start the server aand connect the service
+	
+}
